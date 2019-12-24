93e446c699b98060cdd5c6273ba0911aaf23f1dc
pisomnia <shadine1990@gmail.com>
2019-12-24 02:27:16 -0800

    Update 31_Next Permutation.py

--- Algorithms/Array/31_Next Permutation.py
+++ Algorithms/Array/31_Next Permutation.py
@@ -4,17 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # write your code here
-        for i in range(len(nums)-2, -1, -1):
-            if nums[i] < nums[i+1]:
+        # 先找第一个nums[i]<nums[i+1],然后跟nums[i]右侧比它大的数中的最小值对换，把nums[i]右侧的整个列表翻转
+        if not nums or len(nums)==0:
+            return 
+        n=len(nums)
+        small=-1
+        big=-1
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                small=i
                 break
-        else:
+        if small ==-1:
             nums.reverse()
             return nums
-        for j in range(len(nums)-1, i, -1):
-            if nums[j] > nums[i]:
-                nums[i], nums[j] = nums[j], nums[i]
+        for i in range(n-1,small,-1):
+            if nums[i]>nums[small]:
+                big=i
                 break
-        for j in range(0, (len(nums) - i)//2):
-            nums[i+j+1], nums[len(nums)-j-1] = nums[len(nums)-j-1], nums[i+j+1]
+        nums[small],nums[big] = nums[big],nums[small]
+        for i in range(0,(n-small)//2):
+            nums[small+1+i],nums[n-1-i]=nums[n-1-i],nums[small+1+i]
         return nums
