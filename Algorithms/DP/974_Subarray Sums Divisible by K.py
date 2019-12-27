ad6da455f17ef2b4fab201afe904e31ed11168c1
pisomnia <shadine1990@gmail.com>
2019-12-26 19:34:41 -0800

    Update 974_Subarray Sums Divisible by K.py

--- Algorithms/DP/974_Subarray Sums Divisible by K.py
+++ Algorithms/DP/974_Subarray Sums Divisible by K.py
@@ -4,5 +4,29 @@ class Solution(object):
         :type A: List[int]
         :type K: int
         :rtype: int
+        
+        hashmap={}
+        hashmap[0]=1
+        sum=0
+        for num in A:
+            sum+=num
+            hashmap[sum%K]=hashmap.get(sum%K,0)+1
+        count=0
+        for value in hashmap.values():
+            count+=value*(value-1)/2
+        return count
         """
-#dp[i]代表以i结尾的能被K整除的子数组的最多个数
+        dp=[0]*(len(A)+1)
+        N=len(A)+1
+        prefix_sum=[0]*N
+        res=0
+        for i in range(1,N):
+            prefix_sum[i]=prefix_sum[i-1]+A[i-1]
+        for i in range(1,N):
+            for j in range(i-1,-1,-1):
+                if ((prefix_sum[i] - prefix_sum[j]) % K == 0):
+                    dp[i]=dp[j]+1
+                    res+=dp[i]
+                    break
+        return res
+        
