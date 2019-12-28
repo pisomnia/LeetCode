a72003ea38e1319b0b7a30f4583371b09cfd9f74
pisomnia <shadine1990@gmail.com>
2019-12-28 02:32:12 -0800

    Create 157_Read N Characters Given Read4.py

---
+++ Algorithms/String/157_Read N Characters Given Read4.py
@@ -0,0 +1,35 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf, a list of characters
+    @return an integer
+    def read4(buf):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        idx = 0
+        while n > 0:
+        # read file to buf4
+            buf4 = [""]*4
+            l = read4(buf4)
+        # if no more char in file, return
+            if not l:
+                return idx
+        # write buf4 into buf directly
+            for i in range(min(l, n)):
+                buf[idx] = buf4[i]
+                idx += 1
+                n -= 1
+        return idx
