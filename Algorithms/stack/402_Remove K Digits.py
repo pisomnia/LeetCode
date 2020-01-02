9d1084d40d669f85ee044a63d7c4d14869009c26
pisomnia <shadine1990@gmail.com>
2020-01-02 01:46:27 -0800

    Create 402_Remove K Digits.py

---
+++ Algorithms/stack/402_Remove K Digits.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        if len(num) == k:
+            return '0'
+        if k==0:
+            return num
+        stack = []
+        for n in num:
+            while stack and k and int(stack[-1]) > int(n):
+                stack.pop()
+                k -= 1
+            stack.append(n)
+        while k:
+            stack.pop()
+            k -= 1
+        
+        return str(int("".join(stack)))
+
+        
