b19111543e61244b15bf78fd15dfc1748695cb71
pisomnia <shadine1990@gmail.com>
2020-01-02 01:46:21 -0800

    Create 232_Implement Queue using Stacks.py

---
+++ Algorithms/stack/232_Implement Queue using Stacks.py
@@ -0,0 +1,50 @@
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        """
+        self.stack1.append(x)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
+
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        if self.stack2:
+            return self.stack2[-1]
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2[-1]
+
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return not self.stack1 and not self.stack2
+        
